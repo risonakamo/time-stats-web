788355b9771e5e99ff4c90fede91bdff7c9d94e0
risonakamo <hmykanae@gmail.com>
2024-02-25 01:07:33 -0500

    setting up mirror api functions

---
+++ web/@types/time-stat-api-types.d.ts
@@ -0,0 +1,59 @@
+// typings for time stat server api. all these types should exist on the api side as well
+
+/** collection of tag breakdowns. associated with a list of events.
+ *  key: a tag (not the value, but the tag name)
+ *  value: breakdown for that particular tag */
+type TagBreakdownsDict=Record<string,TagBreakdown>
+
+/** analysis of a tag's values.
+ *  key: a tag value (not the name of the tag, but the unique values of the tag)
+ *  val: the analysis of all events that have that particular value */
+type TagValueAnalysisDict=Record<string,TimeEventAnalysis>
+
+/** an available time data file */
+interface TimeStatDataFile
+{
+    filename:string
+    displayName:string
+}
+
+/** a tag filter */
+interface TagFilter
+{
+    tag:string
+    value:string
+}
+
+/** analysis output for a particular time data file */
+interface TimeDataFile
+{
+    topAnalysis:TimeEventAnalysis
+    tagAnalysis:TagBreakdownsDict
+}
+
+/** analysis of a list of time events */
+interface TimeEventAnalysis
+{
+    totalTime:number
+    averageTime:number
+
+    earliestEventDate:number
+}
+
+/** breakdown analysis for a particular tag */
+interface TagBreakdown
+{
+    tag:string
+
+    valuesAnalysis:TagValueAnalysisDict
+
+    averageTime:number
+    totalTime:number
+}
+
+/** request to get a data file */
+interface GetDataRequest
+{
+    filename:string
+    filters:TagFilter[]
+}
---
+++ web/apis/time-stat-api.ts
@@ -0,0 +1,24 @@
+// api calls with time stat api
+
+import axios from "axios";
+
+const ax=axios.create({
+    baseURL:"http://localhost:4201",
+});
+
+/** get all available data files */
+export async function getAvailableTimeDatas():Promise<TimeStatDataFile[]>
+{
+    return (await ax.get("/data-names")).data;
+}
+
+/** get a target datafile with options */
+export async function getTimeDatafile(filename:string,filters:TagFilter[]):Promise<TimeDataFile>
+{
+    const request:GetDataRequest={
+        filename,
+        filters
+    };
+
+    return (await ax.post("/get-data",request)).data;
+}
