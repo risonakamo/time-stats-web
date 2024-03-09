f33b58a385cdd64093fa5867b92c0a650e22e5c3
risonakamo <hmykanae@gmail.com>
2024-03-09 13:31:13 -0500

    implement active filter display

--- web/components/dataset-info-panel/dataset-info-panel.tsx
+++ web/components/dataset-info-panel/dataset-info-panel.tsx
@@ -14,6 +14,24 @@ export function DatasetInfoPanel(props:DatasetInfoPanelProps):JSX.Element
   return <div className="dataset-info-panel">
     <h1>{props.datasetInfo.displayName}</h1>
 
+    <div className="filters-display">
+      <div className="filter">
+        <div className="strikethrough"></div>
+        <div className="remove-icon"></div>
+        <p className="tag-name">Category</p>
+        <p className="ellipse">...</p>
+        <p>sk2</p>
+      </div>
+
+      <div className="filter">
+        <div className="strikethrough"></div>
+        <div className="remove-icon"></div>
+        <p className="tag-name">Item</p>
+        <p className="ellipse">...</p>
+        <p>3</p>
+      </div>
+    </div>
+
     <div className="info-box">
       <p>Total time: {nanoToHours(props.datafile.topAnalysis.totalTime)}</p>
       <p>Average time: {nanoToHours(props.datafile.topAnalysis.averageTime)}</p>
